8082ece2493b0cb3c2834badabfa70e38b717384
tanmay493 <tanmaylanjewar24874@gmail.com>
2025-11-03 17:21:00 +0530

    funtions

---
+++ 14.py
@@ -0,0 +1,11 @@
+s='python is easy lang.'
+print(max(s))
+print(len(s))
+print(min(s))
+#print(sum(s))  # sum ek aisa function jo string ko support nhi karta
+print(s.swapcase())
+print(s.capitalize())# poore sentence ke first word ke first char. ko capital karega
+print(s.title())#poore sentence jitne words hai unke first char. ko capital karega
+print(s.find('Y')) #index position -1 hai kyunki woh is string me available
+print(s.count('y'))
+print(s.count('Y'))
