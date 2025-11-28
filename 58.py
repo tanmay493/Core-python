3455fab01b228d9700f77c7990c4cfea565e23f0
tanmay493 <tanmaylanjewar24874@gmail.com>
2025-11-28 18:29:08 +0530

    update

--- 58.py
+++ 58.py
@@ -1,3 +1,4 @@
+
 # a=lambda x,y,z:2*x+y+z
 # print(a(1,2,3))
 
@@ -12,6 +13,22 @@
 # n=int(input("enter no."))
 # print(x(n))
 
-x=lambda n:n**2
-n=int(input("enter a no"))
-print(x(n))
+# x=lambda n:n**2
+# n=int(input("enter a no"))
+# print(x(n))
+
+# x=lambda p,q:p+q
+# print(x(5,10))
+
+# n=int(input ("enter a no"))
+# x=lambda n:[i for i in range(1,n+1)]
+# print(x(n))
+
+# n=int(input("enter no."))
+# x=lambda
+
+l=[1,2,3,4,5]
+# print(list(map(lambda n:n**2,l)))
+# print(list(filter(lambda n : 'even'if n%2==0 else None,l)))
+print(list(reduce()))
+
